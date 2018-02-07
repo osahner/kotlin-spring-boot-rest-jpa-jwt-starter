5db68869945529349d51899ad48b80bfcba9bd4d
Oliver Sahner <osahner@medianet.de>
2018-02-07 15:46:12 +0100

    fix: no String.format! chore: update Readme

--- src/main/kotlin/osahner/service/AppUserDetailsService.kt
+++ src/main/kotlin/osahner/service/AppUserDetailsService.kt
@@ -17,7 +17,7 @@ class AppUserDetailsService(val userRepository: UserRepository) : UserDetailsSer
   @Throws(UsernameNotFoundException::class)
   override fun loadUserByUsername(s: String): UserDetails {
     val user = userRepository.findByUsername(s)
-      .orElseThrow { UsernameNotFoundException(String.format("The username %s doesn't exist", s)) }
+      .orElseThrow { UsernameNotFoundException("The username $s doesn't exist") }
 
     val authorities = ArrayList<GrantedAuthority>()
     user.roles!!.forEach { role -> authorities.add(SimpleGrantedAuthority(role.roleName)) }
