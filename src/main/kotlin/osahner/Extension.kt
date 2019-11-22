1cc4a85c81b2aadf060b537b64b9a4b276e49377
Oliver Sahner <osahner@medianet.de>
2019-11-22 16:04:44 +0100

    feat: add Test for Extensions

--- src/main/kotlin/osahner/Extension.kt
+++ src/main/kotlin/osahner/Extension.kt
@@ -2,11 +2,6 @@ package osahner
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.util.*
 import kotlin.reflect.full.memberProperties
 
 fun Any?.writeValueAsString(): String? {
@@ -26,42 +21,6 @@ fun String?.toStringArray(): Collection<String>? {
     } catch (e: Exception) {
       null
     }
-  } else {
-    null
-  }
-}
-
-fun LocalDate?.toDate(): Date? {
-  return if (this != null) {
-    Date.from(this.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())
-  } else {
-    null
-  }
-}
-
-fun Date?.toLocalDate(): LocalDate? {
-  return if (this != null) {
-    Instant.ofEpochMilli(this.time)
-      .atZone(ZoneId.systemDefault())
-      .toLocalDate()
-  } else {
-    null
-  }
-}
-
-fun LocalDateTime?.toDate(): Date? {
-  return if (this != null) {
-    Date.from(this.atZone(ZoneId.systemDefault()).toInstant())
-  } else {
-    null
-  }
-}
-
-fun Date?.toLocalDateTime(): LocalDateTime? {
-  return if (this != null) {
-    Instant.ofEpochMilli(this.time)
-      .atZone(ZoneId.systemDefault())
-      .toLocalDateTime()
   } else {
     null
   }
