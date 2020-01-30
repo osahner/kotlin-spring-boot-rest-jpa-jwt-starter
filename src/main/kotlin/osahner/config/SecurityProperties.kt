4d4676fc2381f30269393e2976f8b2728f2bdbf1
Oliver Sahner <osahner@medianet.de>
2020-01-30 10:19:19 +0100

    chore(deps): update spring-boot@2.2.4.RELEASE

--- src/main/kotlin/osahner/config/SecurityProperties.kt
+++ src/main/kotlin/osahner/config/SecurityProperties.kt
@@ -1,10 +1,12 @@
 package osahner.config
 
+import org.hibernate.validator.constraints.Length
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "jwt-security")
 class SecurityProperties {
-  var secret = "PseudoSecret-Pseudosecret-Please-Use-Ur-Own-Key-PseudoSecret-Pseudosecret"
+  @Length(min = 42, message = "Minimum length for the secret is 42.")
+  var secret = ""
   var expirationTime: Int = 31 // in days
   var tokenPrefix = "Bearer "
   var headerString = "Authorization"
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,10 +5,10 @@ INSERT INTO app_role (id, role_name, description)
 VALUES (2, 'STANDARD_USER', 'Standard User - Has no admin rights');
 
 TRUNCATE TABLE app_user;
--- pasword test1234
+-- password test1234
 INSERT INTO app_user (id, first_name, last_name, password, username)
 VALUES (1, 'Admin', 'Admin', '$2a$10$5AWyzymSnNypg9BkMOyKE.zA05GtRKHCoWimh.q2w.KAO5koBYPM6', 'admin.admin');
--- pasword test1234
+-- password test1234
 INSERT INTO app_user (id, first_name, last_name, password, username)
 VALUES (2, 'John', 'Doe', '$2a$10$5AWyzymSnNypg9BkMOyKE.zA05GtRKHCoWimh.q2w.KAO5koBYPM6', 'john.doe');
 
