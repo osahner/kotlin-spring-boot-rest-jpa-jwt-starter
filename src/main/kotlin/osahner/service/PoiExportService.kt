21c1cc50b6e28bbee3e0e2bb78109e591117b5ae
Oliver Sahner <osahner@medianet.de>
2020-08-13 16:48:18 +0200

    feat: enhance export data

--- src/main/kotlin/osahner/service/PoiExportService.kt
+++ src/main/kotlin/osahner/service/PoiExportService.kt
@@ -1,20 +1,30 @@
 package osahner.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.convertValue
+import org.apache.poi.hssf.usermodel.HSSFDataFormat
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.BorderStyle
 import org.apache.poi.ss.usermodel.Workbook
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.*
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
 
 @Component
 class PoiExportService(
-  @Qualifier("objectMapper") private val mapper: ObjectMapper
+  private val mapper: ObjectMapper
 ) {
   fun buildExcelDocument(
     titel: String? = "Export",
     header: Collection<String>,
+    result: Collection<Any>
+  ) = buildExcelDocument(titel, header.map { it to it.capitalize() }.toMap(), result)
+
+  fun buildExcelDocument(
+    titel: String? = "Export",
+    header: Map<String, String>,
     result: Collection<Any>
   ): Workbook = HSSFWorkbook().apply {
     val sheet = createSheet(titel).apply {
@@ -27,34 +37,51 @@ class PoiExportService(
       setFont(headerFont)
       borderBottom = BorderStyle.MEDIUM
     }
+    val dateStyle = createCellStyle().apply {
+      dataFormat = HSSFDataFormat.getBuiltinFormat("m/d/yy h:mm")
+    }
 
     val createHelper = creationHelper
     var rowNo = 0
     var row = sheet.createRow(rowNo++)
 
-    header.withIndex().forEach { (cellNo, it) ->
+    header.values.withIndex().forEach { (cellNo, it) ->
       row.createCell(cellNo).apply {
-        setCellValue(createHelper.createRichTextString(it.capitalize()))
+        setCellValue(createHelper.createRichTextString(it))
         setCellStyle(headerCellStyle)
       }
     }
 
     result.forEach { entity ->
-      val map = mapper.convertValue<Map<String, Any>>(entity)
-      val list = header.map { map[it] }
+      val list = header.keys.map {
+        readInstanceProperty(entity, it)
+      }
       row = sheet.createRow(rowNo++)
-      list.withIndex().forEach { (cellNo, entity) ->
+      list.withIndex().forEach { (cellNo, cell) ->
         row.createCell(cellNo).apply {
-          when (entity) {
-            is Number -> setCellValue(entity.toDouble())
-            is String -> setCellValue(entity as String?)
-            is Boolean -> setCellValue((entity as Boolean?)!!)
-            is Collection<*> -> setCellValue(entity.joinToString("; "))
-            is Any -> setCellValue(mapper.writeValueAsString(entity))
+          when (cell) {
+            is LocalDate -> {
+              setCellValue(Date.from(cell.atStartOfDay(ZoneId.systemDefault()).toInstant()) as Date)
+              setCellStyle(dateStyle)
+            }
+            is Number -> setCellValue(cell.toDouble())
+            is String -> setCellValue(cell as String?)
+            is Boolean -> setCellValue((cell as Boolean?)!!)
+            is Collection<*> -> setCellValue(cell.joinToString("; "))
+            is Any -> setCellValue(mapper.writeValueAsString(cell))
             else -> setCellValue("") // null -> empty text field
           }
         }
       }
+    }
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  private fun readInstanceProperty(instance: Any, propertyName: String): Any? {
+    return try {
+      (instance::class.memberProperties.first { it.name == propertyName } as KProperty1<Any, *>).get(instance)
+    } catch (e: Exception) {
+      ""
     }
   }
 }
