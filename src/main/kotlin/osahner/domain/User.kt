95964d6253c7297072c0b7b635e1698d033a47ec
Oliver Sahner <osahner@medianet.de>
2020-08-13 16:47:48 +0200

    fix(JPA):  avoid creation of hibernate_sequence, jointable without primary key

--- src/main/kotlin/osahner/domain/User.kt
+++ src/main/kotlin/osahner/domain/User.kt
@@ -21,12 +21,12 @@ class User(
   @Column(name = "last_name")
   var lastName: String? = null,
 
-  @ManyToMany(fetch = FetchType.EAGER)
+  @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
   @JoinTable(
     name = "user_role",
     joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
     inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")]
   )
-  var roles: Collection<Role>? = null
+  var roles: Set<Role>? = null
 )
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
 TRUNCATE TABLE app_role;
 INSERT INTO app_role (id, role_name, description)
 VALUES (1, 'ADMIN_USER', 'Admin User - Has permission to perform admin tasks');
@@ -19,3 +21,14 @@ INSERT INTO user_role(user_id, role_id)
 VALUES (1, 2);
 INSERT INTO user_role(user_id, role_id)
 VALUES (2, 2);
+
+TRUNCATE TABLE address;
+INSERT INTO address (id, name, street, zip, city, email, tel, enabled, lastModified, options, things)
+VALUES (1, 'John Doe', '4429  Jessie Street', '43215', 'Columbus', '9j92thgzfwp@fakemailgenerator.net', '520-891-3434',
+        1, '2020-01-01', '{"option1":"value1"}', '["onething","anotherthing"]');
+
+INSERT INTO address (id, name, street, zip, city, email, tel, enabled, lastModified, options, things)
+VALUES (2, 'Agent Smith', '1730  Polk Street', '85705', 'Tucson', 'd3hf1lbm0z@powerencry.com', '270-466-4222', 1,
+        '2020-01-01', '{"option1":"value1"}', '["onething","anotherthing"]');
+
+SET FOREIGN_KEY_CHECKS = 1;
