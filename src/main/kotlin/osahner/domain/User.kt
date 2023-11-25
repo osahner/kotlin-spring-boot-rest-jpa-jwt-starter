8e802006b60d0fa0cdb60c1b1ec1fe142b29e024
Oliver Sahner <osahner@gmail.com>
2023-11-25 08:40:37 +0100

    fix: renamed join table

--- src/main/kotlin/osahner/domain/User.kt
+++ src/main/kotlin/osahner/domain/User.kt
@@ -41,7 +41,7 @@ class User(
 
   @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.MERGE])
   @JoinTable(
-    name = "user_role",
+    name = "app_user_role",
     joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
     inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")]
   )
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,12 +14,12 @@ VALUES (1, 'Admin', 'Admin', '$2a$10$5AWyzymSnNypg9BkMOyKE.zA05GtRKHCoWimh.q2w.K
 INSERT INTO app_user (id, first_name, last_name, password, username, isUsing2FA, secret)
 VALUES (2, 'John', 'Doe', '$2a$10$5AWyzymSnNypg9BkMOyKE.zA05GtRKHCoWimh.q2w.KAO5koBYPM6', 'john.doe', true, '5TPFV3VGX3EKYAET');
 
-TRUNCATE TABLE user_role;
-INSERT INTO user_role(user_id, role_id)
+TRUNCATE TABLE app_user_role;
+INSERT INTO app_user_role(user_id, role_id)
 VALUES (1, 1);
-INSERT INTO user_role(user_id, role_id)
+INSERT INTO app_user_role(user_id, role_id)
 VALUES (1, 2);
-INSERT INTO user_role(user_id, role_id)
+INSERT INTO app_user_role(user_id, role_id)
 VALUES (2, 2);
 
 TRUNCATE TABLE address;
